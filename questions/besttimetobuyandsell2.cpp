4b7d4b7b0ac488f2710cf3deb8d0bb3f40cc5098
parthnijh <parthnijhawan777@gmail.com>
2025-08-17 20:52:16 +0530

    best time buy and sell 2

---
+++ questions/besttimetobuyandsell2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit=0;
+        int sumprofit=0;
+        int min=prices[0];
+        for(int i=1; i<prices.size(); i++){
+            profit=prices[i]-min;
+            if(profit>0){
+                sumprofit+=profit;
+                min=prices[i];
+            };
+            min=min<prices[i] ? min : prices[i];
+
+        };
+
+        return sumprofit;
+        
+    }
+};
