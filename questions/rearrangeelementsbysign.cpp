f54b8fdc4c56e098fedf33700c727a6e12a23434
parthnijh <parthnijhawan777@gmail.com>
2025-08-21 20:42:31 +0530

    insertion sort fixed and better approach of matrix zeroes done

--- questions/rearrangeelementsbysign.cpp
+++ questions/rearrangeelementsbysign.cpp
@@ -1,22 +1,42 @@
-class Solution {
-public:
-    vector<int> rearrangeArray(vector<int>& nums) {
-        int pos=0;
-        int neg=1;
-        int n=nums.size();
-        vector<int>ans(n,0);
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i]<0){
-                ans[neg]=nums[i];
-                neg+=2;
-            }
-            else{
-                ans[pos]=nums[i];
-                pos+=2;
-            };
-        };
+// equal number of positives and negatives
+// class Solution {
+// public:
+//     vector<int> rearrangeArray(vector<int>& nums) {
+//         int pos=0;
+//         int neg=1;
+//         int n=nums.size();
+//         vector<int>ans(n,0);
+//         for(int i=0; i<nums.size(); i++){
+//             if(nums[i]<0){
+//                 ans[neg]=nums[i];
+//                 neg+=2;
+//             }
+//             else{
+//                 ans[pos]=nums[i];
+//                 pos+=2;
+//             };
+//         };
 
-        return ans;
+//         return ans;
         
-    }
-};
+//     }
+// };
+
+//case 2
+// vector<int>pos={};
+// vector<int>neg={};
+// for(int num : nums){
+//     if(num<0) neg.push_back(num);
+//     else pos.push_back(num);
+
+
+
+// }
+
+// if(pos.size()>neg.size()){
+//     for(int i=0; i<neg.size(); i++){
+
+//         ans[2*i]=pos[i];
+//         ans[2*i+1]=neg[i];
+//     }
+// }
--- sorting/insertion_sort.cpp
+++ sorting/insertion_sort.cpp
@@ -2,14 +2,34 @@
 using namespace std;
 int main(){
     int arr[5]={12,3,7,22,11};
+    //3,12
+    //3,7,12
     int n=sizeof(arr)/sizeof(arr[0]);
-    for(int i=0; i<n; i++){
+    int counter=1;
+    for(int i=1; i<n; i++){
         int j=i;
-        while(j>=0 && arr[j]<arr[j-1]){
-            swap(arr[j],arr[j-1]);
-            j--;
+        
+    
+        while(j>0){
+
+            counter++;
+            if(arr[j]<arr[j-1]){
+                swap(arr[j],arr[j-1]);
+                j--;
+
+            }
+            else{
+                break;
+            }
+            
+            
+
+            
         };
     };
+
+    cout<<counter<<endl;
+
 
     for(int i=0; i<n; i++){
         cout<<arr[i]<<endl;
