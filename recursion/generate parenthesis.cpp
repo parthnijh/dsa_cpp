146870e22364d16a2cb67229a24082397166cffa
parthnijh <parthnijhawan777@gmail.com>
2025-10-22 23:28:07 +0530

    optimized way

--- recursion/generate parenthesis.cpp
+++ recursion/generate parenthesis.cpp
@@ -34,3 +34,30 @@ class Solution {
         
     }
 };
+class Solution {
+public:
+    void helper(vector<string>&ans,string curr,int open,int close,int n){
+        if(curr.size()==2*n){
+            ans.push_back(curr);
+
+            return;
+        }
+        if(open<n){
+            helper(ans,curr+'(',open+1,close,n);
+        }
+        if(close<open){
+            helper(ans,curr+')',open,close+1,n);
+        }
+
+
+    }
+    vector<string> generateParenthesis(int n) {
+        string curr="";
+        vector<string>ans;
+        int open=0;
+        int close=0;
+        helper(ans,curr,open,close,n);
+        return ans;
+        
+    }
+};
